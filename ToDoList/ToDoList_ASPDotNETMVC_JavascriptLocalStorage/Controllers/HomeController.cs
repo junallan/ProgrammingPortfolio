8381d47f0995de9680e8865cb981de8b32928b6e
Jun Allan Parreno <junallan@me.com>
2020-05-12 20:26:08 -0600

    ASP.NET MVC Javscript Local Storage implementation

---
+++ ToDoList/ToDoList_ASPDotNETMVC_JavascriptLocalStorage/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList_ASPDotNETMVC_JavascriptLocalStorage.Models;
+
+namespace ToDoList_ASPDotNETMVC_JavascriptLocalStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
