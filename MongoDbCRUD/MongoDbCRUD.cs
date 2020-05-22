c198f15ef9f52636490db5b878e728bdc2150152
Jun Allan Parreno <junallan@me.com>
2020-05-21 21:10:18 -0600

    Class library separation of CRUD operations

---
+++ MongoDbCRUD/MongoDbCRUD.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDbCRUD
+{
+    public class MongoDatabase
+    {
+                private IMongoDatabase db;
+
+        public MongoDatabase(string databaseName)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(databaseName);
+        }
+
+        public void InsertRecord<T>(string tableName, T record)
+        {
+            var collection = db.GetCollection<T>(tableName);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string tableName)
+        {
+            var collection = db.GetCollection<T>(tableName);
+
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+        }
+
+        [Obsolete]
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            ReplaceOneResult replaceOneResult = collection.ReplaceOne(
+                                new BsonDocument("_id",
+                                                 id),
+                                record,
+                                new UpdateOptions { IsUpsert = true }
+                        );
+            var result = replaceOneResult;
+        }
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);           
+        }
+
+        //public void DeleteAllRecordsExcept(string table, List<Guid> ids)
+        public void DeleteAllRecords<T>(string table)        
+        {
+            var collection = db.GetCollection<T>(table);
+            var result = collection.DeleteMany(new BsonDocument());        
+        }
+
+    }
+}
--- ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/Controllers/HomeController.cs
+++ ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/Controllers/HomeController.cs
@@ -8,23 +8,25 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using ToDoList_ASPDotNETMVC_MongoStorage.Models;
+using MongoDbCRUD;
 
 namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private MongoDatabase  _db;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _db = new MongoDatabase("ToDo");
         }
 
         [HttpGet]
        public JsonResult GetToDoItems() 
         {
-            MongoCRUD db = new MongoCRUD("ToDo");
-            var recs = db.LoadRecords<ToDoItemsModel>("ToDoItems");
+            var recs = _db.LoadRecords<ToDoItemsModel>("ToDoItems");
 
             // foreach(var rec in recs)
             // {
@@ -37,9 +39,7 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
         [HttpPost]
         public JsonResult DeleteAllToDoItems()
         {
-            MongoCRUD db = new MongoCRUD("ToDo");
-
-            db.DeleteAllRecords<ToDoItemsModel>("ToDoItems");
+            _db.DeleteAllRecords<ToDoItemsModel>("ToDoItems");
             //db.LoadRecords<ToDoItemsModel>("ToDoItems").ToList()
             //        .ForEach(x => db.DeleteRecord<ToDoItemsModel>("ToDoItems", x.Id));
 
@@ -49,11 +49,9 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
         [HttpPost]
         public JsonResult DeleteCompletedToDoItems()
         {
-            MongoCRUD db = new MongoCRUD("ToDo");
-
-            db.LoadRecords<ToDoItemsModel>("ToDoItems")
+            _db.LoadRecords<ToDoItemsModel>("ToDoItems")
                     .Where(x => x.State != 0).ToList()
-                    .ForEach(x => db.DeleteRecord<ToDoItemsModel>("ToDoItems", x.Id));
+                    .ForEach(x => _db.DeleteRecord<ToDoItemsModel>("ToDoItems", x.Id));
 
 
             return Json(true);
@@ -62,15 +60,13 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
         [HttpPost]
         public JsonResult SaveToDoItems(ToDoItemsModel item)
         {
-            MongoCRUD db = new MongoCRUD("ToDo");
-
             if(item.Id == Guid.Empty)
             {
-                db.InsertRecord<ToDoItemsModel>("ToDoItems",item);
+                _db.InsertRecord<ToDoItemsModel>("ToDoItems",item);
             }
             else
             {
-                db.UpsertRecord<ToDoItemsModel>("ToDoItems", item.Id, item);
+                _db.UpsertRecord<ToDoItemsModel>("ToDoItems", item.Id, item);
             }
 
             return Json(true);
@@ -99,64 +95,4 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
         public string WorkItemDescription{get;set;}
         public int State{get;set;}
     }
-
-    public class MongoCRUD
-    {
-        private IMongoDatabase db;
-
-        public MongoCRUD(string databaseName)
-        {
-            var client = new MongoClient();
-            db = client.GetDatabase(databaseName);
-        }
-
-        public void InsertRecord<T>(string tableName, T record)
-        {
-            var collection = db.GetCollection<T>(tableName);
-            collection.InsertOne(record);
-        }
-
-        public List<T> LoadRecords<T>(string tableName)
-        {
-            var collection = db.GetCollection<T>(tableName);
-
-            return collection.Find(new BsonDocument()).ToList();
-        }
-
-        public T LoadRecordById<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-
-            return collection.Find(filter).First();
-        }
-
-        [Obsolete]
-        public void UpsertRecord<T>(string table, Guid id, T record)
-        {
-            var collection = db.GetCollection<T>(table);
-
-            ReplaceOneResult replaceOneResult = collection.ReplaceOne(
-                                new BsonDocument("_id",
-                                                 id),
-                                record,
-                                new UpdateOptions { IsUpsert = true }
-                        );
-            var result = replaceOneResult;
-        }
-
-        public void DeleteRecord<T>(string table, Guid id)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("Id", id);
-            collection.DeleteOne(filter);           
-        }
-
-        //public void DeleteAllRecordsExcept(string table, List<Guid> ids)
-        public void DeleteAllRecords<T>(string table)        
-        {
-            var collection = db.GetCollection<T>(table);
-            var result = collection.DeleteMany(new BsonDocument());        
-        }
- }
 }
