c9bb4fa5435e05408a562268ed2f958f19644d59
Jun Allan Parreno <junallan@me.com>
2020-05-30 10:40:39 -0600

    Added Web CLI

--- ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/Controllers/HomeController.cs
+++ ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/Controllers/HomeController.cs
@@ -12,6 +12,23 @@ using MongoDbCRUD;
 
 namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
 {
+  public class CommandLine
+    {
+        public string cmdLine { get; set; }
+
+        // public string[] GetArgs()
+        // {
+        //     //Matches (1 or more chars that are NOT space or ") or (" any # of chars not a " followed by a ")
+        //     // var tokenEx = new Regex(@"[^\s""]+|""[^""]*""");
+
+        //     // return tokenEx.Matches(CmdLine)
+        //     //                      .Cast<Match>()
+        //     //                      .Select(m => m.Value.Replace("\"", ""))  //Remove " from the arg
+        //     //                      .ToArray();
+        //     return null;
+        // }
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -21,6 +38,12 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.Controllers
         {
             _logger = logger;
             _db = new MongoDatabase("ToDo");
+        }
+
+        [HttpPost]
+        public JsonResult WebCLI([FromBody] CommandLine command)
+        {
+            return Json("Test");
         }
 
         [HttpGet]
