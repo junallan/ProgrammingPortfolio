250aa449da6c381e63d0e4ff5660a0db4ccf2e98
Jun Allan Parreno <junallan@me.com>
2020-06-27 19:20:38 -0600

    Help command formatting

--- ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/WebCLI/Commands/Help.cs
+++ ToDoList/ToDoList_ASPDotNETMVC_MongoStorage/WebCLI/Commands/Help.cs
@@ -11,10 +11,11 @@ namespace ToDoList_ASPDotNETMVC_MongoStorage.WebCLI.Commands
             {
                 return new ConsoleErrorResult("No parameters needed");
             }
+            
             string lineBreak = "<br>";
-            string space = "&nbsp;";
-            var consoleResult = new ConsoleResult($"1. 'Delete All'{space}{space}{space}{space}{space}{space}{space}- Deletes all todo items {lineBreak} 2. 'Delete Completed' - Deletes all todo items completed {lineBreak} 3. Save [0] {space}{space}{space}{space}{space}{space}{space}{space}{space}{space}- Save a todo item where [0] is item to save"); 
+            var consoleResult = new ConsoleResult($"<div style='float:left;width:270px;'>1. 'Delete All'</div><div style='width:20px;float:left;'>-</div><div style='float:left;'>Deletes all todo items</div>{lineBreak}<div style='float:left;width:270px;'>2. 'Delete Completed'</div><div style='width:20px;float:left;'>-</div><div style='float:left;'>Deletes all todo items completed</div>{lineBreak}<div style='float:left;width:270px;'>3. Save [0]</div><div style='width:20px;float:left;'>-</div><div style='float:left;'>Save a todo item where [0] is item to save</div>"); 
             consoleResult.isHTML = true;
+            
             return consoleResult;
         }
     }
