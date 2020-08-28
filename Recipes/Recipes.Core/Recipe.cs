890a98aa5a7937a55f3458ef272196ef590d1fcb
ASUS-LAPTOP\Jun <junallan@telus.net>
2020-08-28 08:20:28 -0600

    Added Repice core class

---
+++ Recipes/Recipes.Core/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Recipes.Core
+{
+    public class Recipe
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int CookTimeMinutes { get; set; }
+        public int Servings { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Directions { get; set; }
+    }
+}
