3f18af77073321793770731d239683708cc7ebdd
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-01-14 07:56:18 -0700

    Menu recipe search added ingredient search textbox input and retrieval of input on OnGet method.

--- Recipes/Recipes/Pages/MenuRecipes/Search.cshtml.cs
+++ Recipes/Recipes/Pages/MenuRecipes/Search.cshtml.cs
@@ -4,16 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Recipes.Core;
+using Recipes.Data;
 
 namespace Recipes.Pages.MenuRecipes
 {
     public class SearchModel : PageModel
     {
+        private readonly IRecipeData recipeData;
+        public IEnumerable<Recipe> Recipes { get; set; }
+
+        public SearchModel(IRecipeData recipeData)
+        {
+            this.recipeData = recipeData;
+        }
+
         [BindProperty(SupportsGet = true)]
         public string Ingredients { get; set; }
 
+
+
         public void OnGet()
         {
+            var test = Ingredients;
+
+            //TODO: Add method on IRecipeData for search by Ingredients input
+            Recipes = recipeData.GetAll();
         }
 
         public void OnPost()
