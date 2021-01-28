ddab5033dba75b998192dcba9e6a93f28ce5c226
ASUS-LAPTOP\Jun <junallan@telus.net>
2021-01-28 07:12:41 -0700

    Menu recipe filter, extract common filter method for filter checks.

--- Recipes/Recipes/Pages/MenuRecipes/List.cshtml.cs
+++ Recipes/Recipes/Pages/MenuRecipes/List.cshtml.cs
@@ -29,26 +29,11 @@ namespace Recipes.Pages.MenuRecipes
             FilterValue[] filters = new FilterValue[menuRecipeSearchModel.CountOfFiltersApplied];
 
             int filterCount = 0;
-            //TODO: Refactor repetition??
-            if (menuRecipeSearchModel.IsCookTimeEntered)
-            {
-                filters[filterCount++] = new FilterValue { FilterType = FilterType.EQUAL, ColumnName = "CookTimeMinutes", Values = new List<string> { menuRecipeSearchModel.CookTimeSelected } };
-            }
-
-            if (menuRecipeSearchModel.IsRecipeEntered)
-            {
-                filters[filterCount++] = new FilterValue { FilterType = FilterType.LIKE, ColumnName = "Name", Values = new List<string> { menuRecipeSearchModel.RecipeNameSelected } };
-            }
-
-            if (menuRecipeSearchModel.IsIngredientsEntered)
-            {
-                filters[filterCount++] = new FilterValue { FilterType = FilterType.IN, ColumnName = "Ingredients", Values = menuRecipeSearchModel.IngredientsSelected };
-            }
-
-            if (menuRecipeSearchModel.IsCategoryEntered)
-            {
-                filters[filterCount++] = new FilterValue { FilterType = FilterType.EQUAL, ColumnName = "CategoryId", Values = new List<string> { menuRecipeSearchModel.CategorySelectedId } };
-            }
+ 
+            filterCount = ApplyFilter(menuRecipeSearchModel.IsCookTimeEntered, FilterType.EQUAL, "CookTimeMinutes", new List<string> { menuRecipeSearchModel.CookTimeSelected }, filters, filterCount);
+            filterCount = ApplyFilter(menuRecipeSearchModel.IsRecipeEntered, FilterType.LIKE, "Name", new List<string> { menuRecipeSearchModel.RecipeNameSelected }, filters, filterCount);
+            filterCount = ApplyFilter(menuRecipeSearchModel.IsIngredientsEntered, FilterType.IN, "Ingredients", menuRecipeSearchModel.IngredientsSelected, filters, filterCount);
+            filterCount = ApplyFilter(menuRecipeSearchModel.IsCategoryEntered, FilterType.EQUAL, "CategoryId", new List<string> { menuRecipeSearchModel.CategorySelectedId }, filters, filterCount);
 
             recipesFiltered = this.recipeData.GetByOr(filters);
 
@@ -58,6 +43,16 @@ namespace Recipes.Pages.MenuRecipes
             Recipes = recipesFiltered;
 
             return Page();
+        }
+
+        private static int ApplyFilter(bool isFilterEnabled, FilterType filterType, string columnName, List<string> values, FilterValue[] filters, int filterCount)
+        {
+            if (isFilterEnabled)
+            {
+                filters[filterCount++] = new FilterValue { FilterType = filterType, ColumnName = columnName, Values = values };
+            }
+
+            return filterCount;
         }
     }
 }
